5292905587d1f3d796328175d0cbaf6fb28572a0
Ryo Munakata <afpacket@gmail.com>
2017-06-19 01:55:40 +0900

    Trivial documentation fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 
-//! Filesystems library using 9P2000.L protocol, an extended variant of 9P from the Plan 9.
+//! Filesystems library using 9P2000.L protocol, an extended variant of 9P from Plan 9.
 //!
 //! 9P protocol is originally developed for Plan 9 distributed OS.
 //! As it's extendable and suitable for filesystems 9P is ported to Linux.
-//! However, 9P protocol lacks Linux or unix specific features,
+//! However, 9P protocol lacks Linux or Unix specific features,
 //! which is the problem for developing serious filesystems.
 //!
 //! 9P2000.L is an extended variant protocol of 9P for Linux.
